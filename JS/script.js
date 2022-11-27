8aaecadc18aa2e7e549d6f2603d95256e99ee265
BogartDME <edwin.pietrowski@gmail.com>
2022-11-26 20:44:02 -0500

    minor changes to CSS, JS, and HTML

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,13 @@
 //create fetch for weather API
 // var userInput= querySelector(".city-search");
-var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Philadelphia&appid=b01c800a881fa88d5931029a8b88525c"
+//current day weather request url below 
+const requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}";
+const APIKey = "12341234123412341234123412341234";
+// var responseText = document.getElementById('response-text');
+const citySearch = document.getElementById("city-search");
 
-var responseText = document.getElementById('response-text');
-
-function getApi(requestUrl) {
-  fetch(requestUrl)
+function get5day(requestUrl) {
+  fetch(requestUrl + APIKey)
     .then(function (response) {
       console.log(response);
       if (response.status === 200) {
@@ -14,7 +16,9 @@ function getApi(requestUrl) {
   });
 }
 
-getApi(requestUrl);
+get5day(requestUrl);
+
+
 
 // element.addEventListener("click", myFunction);
 
@@ -24,17 +28,11 @@ getApi(requestUrl);
 //   document.getElementById(".search").addEventListener("click", userInput);
 // }
 
-
-// fetch('api.openweathermap.org/data/2.5/forecast?q={Philadelphia}&appid=b01c800a881fa88d5931029a8b88525c')
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
-
 // 5 day call api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={b01c800a881fa88d5931029a8b88525c}
 
 //call API for current weather
 
 //call API for five day forecast
+
 //create a function for local storage
 
-
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={b01c800a881fa88d5931029a8b88525c}
